3bedf5d1e5b5ffa35e5d9dc0a4ba470aa0b85a25
Daniel-Teseira <daniel.teseira@alu.frt.utn.edu.ar>
2024-08-28 21:41:59 -0300

    se agrego carpetas y dependencias

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import React from 'react';
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
+import Home from './pages/home/Home';
+import Error from './pages/error/Error404';
+import Contact from './pages/contact/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar name='nav'/>
+
+      <Routes>
+        <Route index element={<Navigate to='/home' />} />
+        <Route path='/home' element={ <Home /> } />
+        <Route path='/error' element={ <Error /> } />
+        <Route path='*' element={ <Error/>} />
+        <Route path='/contact' element={ <Contact/> }/>  
+      </Routes>
+
+      <Footer/>
+    </>
   );
 }
 
