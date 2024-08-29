3ab7a8c9c6be59090dadac3fd9e155aef6947ef6
Daniel-Teseira <daniel.teseira@alu.frt.utn.edu.ar>
2024-08-29 10:17:01 -0300

    se agrego footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,134 @@
 import React from 'react'
+import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className='info-pie d-fleX col-12'>
+    <div className='grupo-1'>
+      <div className='d-flex flex-column'>
+        <div className='info-1 d-flex flex-column col-12'>
+          <p>
+            LLEGASTE HASTA EL FINAL
+          </p>
+          <h3>
+            ¿Necesitas reparar tu equipo?
+          </h3>
+        </div>
+      </div>
+
+      <div className='d-flex flex-row col-12'>
+        <div className='info-2 d-flex flex-row col-6'>
+          <div className='info-num d-flex flex-column col-6'>
+            <p>
+              San Miguel de Tucumán
+            </p>
+            <strong>
+              (381)-5694570
+            </strong>
+          </div>
+
+          <div className='info-ubi d-flex flex-column col-6'>
+            <strong>
+              Lunes a Viernes de 10 a 17hs
+            </strong>
+            <p>
+              Suipacha 217
+            </p>
+            <>
+              Ciudad Autónoma de Buenos Aires
+            </>
+            <p>
+              CP1008
+            </p>
+          </div>
+        </div>
+
+        <div className='info-link d-flex flex-row col-6'>
+          <div className='link1 d-flex flex-column col-6'>
+            <div className=''>
+              <h2>
+                Links Útiles
+              </h2>
+            </div>
+
+            <div>
+              <ul>
+                <li>
+                  <a className='text-decoration-none text-white' href='https://www.supertek.com.ar/club/'>
+                    Beneficios al Gremio
+                  </a>
+                </li>
+
+                <li>
+                  <a className='text-decoration-none text-white' href='https://www.supertek.com.ar/club/'>
+                    Club Supertek
+                  </a>
+                </li>
+
+                <li>
+                  <a className='text-decoration-none text-white' href='https://www.supertek.com.ar/contacto'>
+                    Contacto
+                  </a>
+                </li>
+
+                <li>
+                  <a className='text-decoration-none text-white' href='https://www.supertek.com.ar/glosario/staking/'>
+                    Glosario
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='link2 d-flex flex-column col-6'>
+            <div className=''>
+              <div>
+                <h2>
+                  Más Links Útiles
+                </h2>
+              </div>
+
+              <div>
+                <ul>
+                  <li>
+                    <a className='text-decoration-none text-white' href='https://www.supertek.com.ar/blog'>
+                      Blog
+                    </a>
+                  </li>
+
+                  <li>
+                    <a className='text-decoration-none text-white'
+                      href='https://www.supertek.com.ar/trabaja-con-nosotros'>
+                      Trabajá con Nosotros
+                    </a>
+                  </li>
+
+                  <li>
+                    <a className='text-decoration-none text-white'
+                      href='https://www.supertek.com.ar/terminos-y-condiciones'>
+                      Términos y Condiciones
+                    </a>
+                  </li>
+
+                  <li>
+                    <a className='text-decoration-none text-white' href=''>
+                      Vendé tu equipo
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div className='grupo-2 d-flex py-2 align-items-center justify-content-center text-white'>
+      <small>&copy; 2024 <b>Reparacion de Computadoras.</b> Todos los Derechos
+        Reservados</small>
+    </div>
+  </section>
   )
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,15 +5,15 @@ const NavBar = () => {
   return (
   <>
   <section>
-      <ul className="d-flex flex-row align-items-center lista col-12 justify-content-center">
+      <ul className='d-flex flex-row align-items-center lista col-12 justify-content-center'>
         <li>
           <section className='a1a1'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-              className="bi bi-telephone-fill a1b2" viewBox="0 0 16 16">
-              <path fillRule="evenodd"
-                d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor'
+              className='bi bi-telephone-fill a1b2' viewBox='0 0 16 16'>
+              <path fillRule='evenodd'
+                d='M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z' />
             </svg>
-            <a className='a1a2' href="https://www.google.com.ar/">
+            <a className='a1a2' href='https://www.google.com.ar/'>
               Telefono
             </a>
           </section>
@@ -21,12 +21,12 @@ const NavBar = () => {
 
         <li>
           <section className='a1a1'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope"
-              viewBox="0 0 16 16">
+            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-envelope'
+              viewBox='0 0 16 16'>
               <path
-                d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+                d='M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z' />
             </svg>
-            <a className='a1a2' href="https://www.google.com.ar/">
+            <a className='a1a2' href='https://www.google.com.ar/'>
               Gmail
             </a>
           </section>
@@ -34,11 +34,11 @@ const NavBar = () => {
 
         <li>
           <section className='a1a1'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-              className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-              <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor'
+              className='bi bi-geo-alt-fill' viewBox='0 0 16 16'>
+              <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
             </svg>
-            <a className='a1a2' href="https://www.google.com.ar/">
+            <a className='a1a2' href='https://www.google.com.ar/'>
               Ubicación
             </a>
           </section>
@@ -48,64 +48,64 @@ const NavBar = () => {
 
     <nav>
       <section>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="nav1 container-fluid">
-            <a className="navbar-brand" href="https://www.google.com.ar/">
+        <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+          <div className='nav1 container-fluid'>
+            <a className='navbar-brand' href='https://www.google.com.ar/'>
               Reparaciones de Computadoras
             </a>
 
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-              aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
+            <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
+              aria-controls='navbarNavDropdown' aria-expanded='false' aria-label='Toggle navigation'>
+              <span className='navbar-toggler-icon'></span>
             </button>
 
-            <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="https://www.google.com.ar/">
+            <div className='collapse navbar-collapse justify-content-end' id='navbarNavDropdown'>
+              <ul className='navbar-nav'>
+                <li className='nav-item'>
+                  <a className='nav-link active' aria-current='page' href='https://www.google.com.ar/'>
                     Home
                   </a>
                 </li>
 
-                <li className="nav-item">
-                  <a className="nav-link" href="https://www.google.com.ar/">
+                <li className='nav-item'>
+                  <a className='nav-link' href='https://www.google.com.ar/'>
                     Nosotros
                   </a>
                 </li>
 
-                <li className="nav-item">
-                  <a className="nav-link" href="https://www.google.com.ar/">
+                <li className='nav-item'>
+                  <a className='nav-link' href='https://www.google.com.ar/'>
                     Preguntas Frecuentes
                   </a>
                 </li>
 
-                <li className="nav-item">
-                  <a className="nav-link" href="https://www.google.com.ar/">
+                <li className='nav-item'>
+                  <a className='nav-link' href='https://www.google.com.ar/'>
                     Contacto
                   </a>
                 </li>
 
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="https://www.google.com.ar/" role="button" data-bs-toggle="dropdown"
-                    aria-expanded="false">
+                <li className='nav-item dropdown'>
+                  <a className='nav-link dropdown-toggle' href='https://www.google.com.ar/' role='button' data-bs-toggle='dropdown'
+                    aria-expanded='false'>
                     Servicios
                   </a>
 
-                  <ul className="dropdown-menu">
+                  <ul className='dropdown-menu'>
                     <li>
-                      <a className="dropdown-item" href="https://www.google.com.ar/">
+                      <a className='dropdown-item' href='https://www.google.com.ar/'>
                         Action
                       </a>
                     </li>
 
                     <li>
-                      <a className="dropdown-item" href="https://www.google.com.ar/">
+                      <a className='dropdown-item' href='https://www.google.com.ar/'>
                         Another action
                       </a>
                     </li>
 
                     <li>
-                      <a className="dropdown-item" href="https://www.google.com.ar/">
+                      <a className='dropdown-item' href='https://www.google.com.ar/'>
                         Something else here
                       </a>
                     </li>
