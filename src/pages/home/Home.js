4aea347c151a46367e6a0d10a89a3d991f283eee
Daniel Martin Teseira <daniel.teseira@alu.frt.utn.edu.ar>
2025-07-02 20:17:42 -0300

    se agrego carrusel con imagenes

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,65 @@
 import React from 'react';
-import ContactForm from '../../utils/formulario';
+//import ContactForm from '../../utils/formulario';
+import './Home.css';  
+import img1 from '../../images/carrusel/pc1.png';
+import img2 from '../../images/carrusel/pc2.png';
+import img3 from '../../images/carrusel/redes1.png';
+import img4 from '../../images/carrusel/redes2.png';
+
 
 const Home = () => {
   return (
     <>
-      <section class='contacto d-flex flex-wrap justify-content-evenly'>
+      <section class='carrusel'>
+        <div id='carouselhome' class='carousel slide carousel-fade' data-bs-ride='carousel'>
+          <div class='carousel-indicators'>
+            <button type='button' data-bs-target='#carouselhome' data-bs-slide-to='0' class='active' aria-current='true' aria-label='Slide 1'></button>
+            <button type='button' data-bs-target='#carouselhome' data-bs-slide-to='1' aria-label='Slide 2'></button>
+            <button type='button' data-bs-target='#carouselhome' data-bs-slide-to='2' aria-label='Slide 3'></button>
+            <button type='button' data-bs-target='#carouselhome' data-bs-slide-to='3' aria-label='Slide 4'></button>
+          </div>
+          <div class='carousel-inner'>
+            <div class='carousel-item active'>          
+              <img src={img1} class='d-block w-100' alt='Reparacion PC 1'></img>
+              <div class='carousel-caption d-none d-md-block'>
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+              </div>
+            </div>
+            <div class='carousel-item'>
+              <img src={img2} class='d-block w-100' alt='Reparacion PC 2'></img>
+              <div class='carousel-caption d-none d-md-block'>
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+            <div class='carousel-item'>
+              <img src={img3} class='d-block w-100' alt='Instalacion Redes 1'></img>
+              <div class='carousel-caption d-none d-md-block'>
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+              </div>
+            </div>
+            <div class='carousel-item'>
+              <img src={img4} class='d-block w-100' alt='Instalacion Redes 2'></img>
+              <div class='carousel-caption d-none d-md-block'>
+                <h5>Fourth slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+              </div>
+            </div>
+          </div>
+          <button class='carousel-control-prev' type='button' data-bs-target='#carouselhome' data-bs-slide='prev'>
+            <span class='carousel-control-prev-icon' aria-hidden='true'></span>
+            <span class='visually-hidden'>Previous</span>
+          </button>
+          <button class='carousel-control-next' type='button' data-bs-target='#carouselhome' data-bs-slide='next'>
+            <span class='carousel-control-next-icon' aria-hidden='true'></span>
+            <span class='visually-hidden'>Next</span>
+          </button>
+        </div>
+      </section>
+   {/*
+      <section class='contacto d-flex flex-wrap justify-content-evenly container my-5'>
         <div class='d-flex flex-column col-5 '>
           <h2 class='col-12'>¡Estamos para Ayudarte!</h2>
 
@@ -47,7 +102,7 @@ const Home = () => {
                     target='_blank'
                     rel='noopener noreferrer'
                   >
-                    Pasaje Baltazar Aguirre 789
+                    Coronel Zelaya 451, Duplex 7
                     <br />
                     San Miguel de Tucumán, Tucumán
                   </a>
@@ -108,8 +163,8 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </section>
-
+      </section>*/}
+    
       <section className='preguntas'>
         <div className='accordion accordion-flush' id='accordionFlushExample'>
           <div className='accordion-item'>
@@ -555,8 +610,8 @@ const Home = () => {
           Servicio tecnico de Lenovo, Dell, Asus, Acer, HP, etc.
         </div>
 
-        <div className="overflow-hidden whitespace-nowrap">
-         <p className="animate-marquee">
+        <div className='overflow-hidden whitespace-nowrap'>
+         <p className='animate-marquee'>
             servicio tecnico lenovo y exo, reparacion de laptops dell, servicio
             tecnico notebook hp, reparaciones acer, reparar asus, soporte
             técnico vaio reparar hp y toshiba satellite
@@ -571,7 +626,7 @@ const Home = () => {
           </marquee>
         </div>
       </section>
-
+{/*
       <section className='nosotros py-2'>
         <div>
           <div>
@@ -850,7 +905,7 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section>*/}
 
       <section className='servicios'>
         <div className='d-flex justify-content-center'>
